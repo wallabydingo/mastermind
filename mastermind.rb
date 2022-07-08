6faba21a787d7d6ec18b77a741697c57b9bdd641
Wallaby Dingo <wallabydingo@me.com>
2022-07-08 22:05:14 +1000

    shuffle feedback, make prettier

--- mastermind.rb
+++ mastermind.rb
@@ -16,6 +16,12 @@ class Mastermind
     @code
   end
 
+  def show_code
+    puts "╔═══╦═══╦═══╦═══╗"
+    puts "║ #{self.code[0]} ║ #{self.code[1]} ║ #{self.code[2]} ║ #{self.code[3]} ║"
+    puts "╚═══╩═══╩═══╩═══╝"
+  end
+
   def guess_number
     @guess_number
   end
@@ -28,8 +34,26 @@ class Mastermind
     @guesses
   end
 
+  def show_this_guess(guess_number)
+    puts "╔═══╦═══╦═══╦═══╗"
+    puts "║ #{self.guesses[guess_number][0]} ║ #{self.guesses[guess_number][1]} ║ #{self.guesses[guess_number][2]} ║ #{self.guesses[guess_number][3]} ║"
+    puts "╚═══╩═══╩═══╩═══╝"
+  end
+
+  def show_this_feedback(guess_number)
+    puts "╭───┬───╮"
+    puts "│ #{self.feedback[guess_number][0]} │ #{self.feedback[guess_number][1]} │"
+    puts "├───┼───┤"
+    puts "│ #{self.feedback[guess_number][2]} │ #{self.feedback[guess_number][3]} │"
+    puts "╰───┴───╯"
+  end
+
   def feedback
     @feedback
+  end
+
+  def guess_right?
+    self.feedback[self.guess_number - 1] == ["▓", "▓", "▓", "▓"]
   end
 
   def computer_new_code
@@ -55,7 +79,7 @@ class Mastermind
   def generate_feedback
     4.times do |i|
       if !self.code.include?(@guesses[self.guess_number-1][i])
-        @feedback[self.guess_number-1][i] = ""
+        @feedback[self.guess_number-1][i] = "◯"
         next
       elsif self.code[i] == @guesses[self.guess_number-1][i]
         @feedback[self.guess_number-1][i] = "▓" 
@@ -64,30 +88,29 @@ class Mastermind
         @feedback[self.guess_number-1][i] = "░"
       end
     end
+    #@feedback[self.guess_number-1].shuffle!
   end
 
 end
 
-game = Mastermind.new()
+game = Mastermind.new
 game.computer_new_code
-
-p game.code
-
-game.increase_guess_number
-p game.guess_number
-game.choose_pegs
-p game.guesses
-game.generate_feedback
-p game.feedback
-
-game.increase_guess_number
-p game.guess_number
-game.choose_pegs
-p game.guesses
-game.generate_feedback
-p game.feedback
+#p game.code
 
 
+while game.guess_number < 12
+  game.increase_guess_number
+  puts '', "** GUESS NUMBER #{game.guess_number} of 12 **"
+  game.choose_pegs
+  game.show_this_guess(game.guess_number - 1)
+  game.generate_feedback
+  game.show_this_feedback(game.guess_number - 1)
+  game.show_code
+  if game.guess_right?
+    puts '', "You solved it with just #{game.guess_number} guesses! Congratulations!", ''
+    break
+  end
+end
 
-
-
+puts '', '*** CODE ***'
+game.show_code
